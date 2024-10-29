d41505fe187dfb0224a0f6779526e4e42666fdbc
Gracie <gracie.sharma@renegadeinsurance.com>
2024-10-29 08:02:14 +0545

    set up radix ui

---
+++ app/issues/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "@radix-ui/themes";
+
+const IssuePage = () => {
+  return (
+    <div>
+      <Button>New Issue</Button>
+    </div>
+  );
+};
+
+export default IssuePage;
